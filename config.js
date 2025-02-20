3ac67c438e1236003f244a42a031f084c3671f02
Mike Street <mikestreety@gmail.com>
2025-02-20 12:43:11 +0000

    feat: Combine all JS rules - makes it easier to override

--- config.js
+++ config.js
@@ -5,46 +5,60 @@ import globals from 'globals';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-import stylistic from '@stylistic/eslint-plugin'
+import stylistic from '@stylistic/eslint-plugin';
 
 export default {
 	'gitignore': gitignore(),
 
 	'global': {
 		ignores: [
+			'**/Configuration/**',
 			'**/lib/**',
 			'**/libs/**',
 			'**/Resources/Public/**',
 			'**/snapshots/**',
 			'**/TypoScript/**',
-			'**/Configuration/**'
 		]
 	},
 
 	// lint JSON files
 	'json': {
-		files: ['*.json', '**/*.json'],
-		ignores: ['package-lock.json'],
+		files: [
+			'*.json',
+			'**/*.json'
+		],
+		ignores: [
+			'package-lock.json',
+		],
 		language: 'json/json',
 		...json.configs.recommended,
 	},
 
 	// lint JSONC files
 	'jsonc': {
-		files: ['*.jsonc', '**/*.jsonc'],
+		files: [
+			'*.jsonc',
+			'**/*.jsonc'
+		],
 		language: 'json/jsonc',
 		...json.configs.recommended,
 	},
 
 	// lint JSON5 files
 	'json5': {
-		files: ['*.json5', '**/*.json5'],
+		files: [
+			'*.json5',
+			'**/*.json5'
+		],
 		language: 'json/json5',
 		...json.configs.recommended,
 	},
 
 	'markdown': {
-		files: ['*.md', '**/*.md'],
+		files: [
+			'*.md',
+			'**/*.md'
+		],
 		plugins: {
 			markdown
 		},
@@ -54,79 +68,48 @@ export default {
 		}
 	},
 
-	'playwright': {
-		...playwright.configs['flat/recommended'],
-		files: [
-			'playwright.config.ts',
-			'**/*.unit.ts',
-			'**/*.spec.ts',
-			'**/*.test.ts',
-			'**/Fixtures/*.ts',
-		],
-		rules: {
-			...playwright.configs['flat/recommended'].rules,
-			'playwright/no-conditional-in-test': 'off', // Disallow conditional logic in tests
-			'playwright/no-conditional-expect': 'off', // Disallow conditional logic in tests
-			'playwright/valid-title': [
-				'error',
-				{
-					'ignoreTypeOfTestName': true // Allow variables as test names
-				}
-			]
-		},
-	},
-
-	'unicorn': {
-		...eslintPluginUnicorn.configs.recommended,
-		files: [
-			'*.js',
-			'**/*.js',
-			'*.ts',
-			'**/*.ts'
-		],
-		rules: {
-			...eslintPluginUnicorn.configs.recommended.rules,
-			'unicorn/prefer-global-this': 'warn'
-		}
-	},
-
-	'stylistic': {
-		...stylistic.configs.recommended,
-		files: [
-			'*.js',
-			'**/*.js',
-			'*.ts',
-			'**/*.ts'
-		],
-		rules: {
-			...stylistic.configs.recommended.rules,
-			'@stylistic/brace-style': 'error',
-			'@stylistic/indent': ['error', 'tab'],
-			'@stylistic/no-tabs': [
-				'error',
-				{
-					allowIndentationTabs: true
-				}
-			],
-			'@stylistic/operator-linebreak': ['error', 'after'],
-			'@stylistic/semi': 'error',
-			'@stylistic/space-before-function-paren': ['error', 'never'],
-		}
-	},
-
 	'js': {
 		...js.configs.recommended,
+		...stylistic.configs.recommended,
+		...eslintPluginUnicorn.configs.recommended,
+		...playwright.configs['flat/recommended'],
 		files: [
 			'*.js',
 			'**/*.js',
-			'*.ts',
-			'**/*.ts'
+			'*.mjs',
+			'**/*.mjs',
+			'*.cjs',
+			'**/*.cjs',
+			'**/*.spec.cts',
+			'**/*.spec.mts',
+			'**/*.spec.ts',
+			'**/*.test.cts',
+			'**/*.test.mts',
+			'**/*.test.ts',
+			'**/*.unit.cts',
+			'**/*.unit.mts',
+			'**/*.unit.ts',
+			'**/Fixtures/*.cts',
+			'**/Fixtures/*.mts',
+			'**/Fixtures/*.ts',
+			'playwright.config.cts',
+			'playwright.config.mts',
+			'playwright.config.ts',
 		],
+
+		plugins: {
+			'@stylistic': stylistic,
+			unicorn: eslintPluginUnicorn,
+			playwright,
+		},
+
 		languageOptions: {
 			ecmaVersion: 2022,
 			sourceType: 'module',
 			globals: {
 				...js.configs.recommended.globals,
+				...eslintPluginUnicorn.configs.recommended.globals,
+
 				...globals.browser,
 				...globals.node,
 				...globals.jquery,
@@ -138,8 +121,8 @@ export default {
 			},
 		},
 		rules: {
+			// ESLint
 			...js.configs.recommended.rules,
-
 			'capitalized-comments': [
 				'error',
 				'always',
@@ -159,6 +142,34 @@ export default {
 			'prefer-object-has-own': 'error',
 			'prefer-template': 'warn',
 			'radix': ['error', 'as-needed'],
+
+			...playwright.configs['flat/recommended'].rules,
+			'playwright/no-conditional-in-test': 'off', // Disallow conditional logic in tests
+			'playwright/no-conditional-expect': 'off', // Disallow conditional logic in tests
+			'playwright/valid-title': [
+				'error',
+				{
+					'ignoreTypeOfTestName': true // Allow variables as test names
+				}
+			],
+
+			// Unicorn
+			...eslintPluginUnicorn.configs.recommended.rules,
+			'unicorn/prefer-global-this': 'warn',
+
+			// Stylistic
+			...stylistic.configs.recommended.rules,
+			'@stylistic/brace-style': 'error',
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/no-tabs': [
+				'error',
+				{
+					allowIndentationTabs: true
+				}
+			],
+			'@stylistic/operator-linebreak': ['error', 'after'],
+			'@stylistic/semi': 'error',
+			'@stylistic/space-before-function-paren': ['error', 'never'],
 		}
 	}
 };
